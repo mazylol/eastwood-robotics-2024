8087d50cdde1087156dd5c69cc5c9bdf90a26c53
Landon <landonp95@gmail.com>
2024-09-12 20:38:05 +0000

    Pending changes exported from your codespace

---
+++ src/components/Dropdown.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	type Item = {
+		title: string;
+		url: string;
+	};
+
+	export let title: string;
+	export let links: Item[] = [];
+</script>
+
+<li>
+	<details>
+		<summary>{title}</summary>
+		<ul class="p-2 bg-base-100 text-black">
+			{#each links as item}
+				<li>
+					<a href={item.url}>{item.title}</a>
+				</li>
+			{/each}
+		</ul>
+	</details>
+</li>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Navbar from "../components/Navbar.svelte";
+
+    import "../app.css";
+</script>
+
+<div class="min-h-screen bg-neutral-700">
+    <Navbar />
+    <slot />
+</div>
