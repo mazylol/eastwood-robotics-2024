63f1567824c827b9eff583232b9313a3adf18ec1
Landon <landonp95@gmail.com>
2024-09-19 01:52:48 +0000

    banner and stuff

---
+++ src/lib/scroll.ts
@@ -0,0 +1,5 @@
+export function smoothScroll(id: string) {
+    document.querySelector(id)?.scrollIntoView({
+        behavior: "smooth",
+    });
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { smoothScroll } from '$lib/scroll';
+    import MdiArrowDown from '~icons/mdi/arrow-down';
+</script>
+
+<div class="hero min-h-screen" style="background-image: url(/moon.jpg);">
+    <div class="hero-overlay bg-opacity-50"></div>
+    <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold text-white">
+            Eastwood Robotics
+        </h1>
+        <p class="mb-5">
+            At Eastwood Robotics we strive to be the best in robotics
+            technologies
+        </p>
+        <button
+                    id="herobutton"
+                    on:click={() => {
+                        smoothScroll("#therobot");
+                    }}
+                    class="btn btn-primary flex flex-row mx-auto"
+                >
+                    <p class="pt-2 py-2">Get Started</p>
+                    <MdiArrowDown
+                        class="text-white w-8 animate-bounce mt-2"
+                    />
+                </button>
+    </div>
+</div>
+<section class="bg-neutral-300">
+    <h1 id="therobot" class="text-5xl py-8 text-center font-bold">
+        The Robot
+    </h1>
+</section>
