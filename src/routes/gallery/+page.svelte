80e4d666422ee3f2cb3a25a4a1c57ff85eaa574e
Mazy <landonp95@gmail.com>
2024-10-14 17:31:49 +0000

    conversions and more pictures

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,7 +1,7 @@
 <main title="Gallery">
     <h1 class="text-center mt-16 text-6xl font-bold">Gallery</h1>
     <div class="mx-4 mt-16">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             <div>
                 <img src="/gallery/dom-and-layla.jpg" alt="Dominic and Layla working on parts for the course" class="w-full" />
             </div>
@@ -10,6 +10,12 @@
             </div>
             <div>
                 <img src="/gallery/IMG_2995.jpg" alt="Nathan working" class="w-full -rotate-90" />
+            </div>
+            <div>
+                <img src="/gallery/layla-and-cad.jpg" alt="Layla working on making gears in Solidworks" class="w-full" />
+            </div>
+            <div>
+                <img src="/gallery/IMG_2979.jpg" alt="Drawing of our ball picker upper" class="w-full rotate-180" />
             </div>
         </div>
     </div>
