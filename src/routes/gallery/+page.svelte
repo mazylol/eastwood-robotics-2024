a1a3281576cac765454c67e9d75123bf3671cf4d
Mazy <landonp95@gmail.com>
2024-10-14 12:58:12 +0000

    images, whole bunch of HEIC, which is not compatible in browser

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -5,6 +5,12 @@
             <div>
                 <img src="/gallery/dom-and-layla.jpg" alt="Dominic and Layla working on parts for the course" class="w-full" />
             </div>
+            <div>
+                <img src="/gallery/IMG_2909.jpg" alt="A smiling Jeb" class="w-full -rotate-90" />
+            </div>
+            <div>
+                <img src="/gallery/IMG_2995.jpg" alt="Nathan working" class="w-full -rotate-90" />
+            </div>
         </div>
     </div>
 </main>
