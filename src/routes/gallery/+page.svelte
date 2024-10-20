dbc1eeb19189072deaff0fa69c1d1adcac68d1b2
Landon <landonp95@gmail.com>
2024-10-20 14:19:31 -0400

    revamp page + more pictures

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,22 +1,23 @@
 <main title="Gallery">
-    <h1 class="text-center mt-16 text-6xl font-bold">Gallery</h1>
-    <div class="mx-4 mt-16">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            <div>
-                <img src="/gallery/dom-and-layla.jpg" alt="Dominic and Layla working on parts for the course" class="w-full" />
-            </div>
-            <div>
-                <img src="/gallery/IMG_2909.jpg" alt="A smiling Jeb" class="w-full -rotate-90" />
-            </div>
-            <div>
-                <img src="/gallery/IMG_2995.jpg" alt="Nathan working" class="w-full -rotate-90" />
-            </div>
-            <div>
-                <img src="/gallery/layla-and-cad.jpg" alt="Layla working on making gears in Solidworks" class="w-full" />
-            </div>
-            <div>
-                <img src="/gallery/IMG_2979.jpg" alt="Drawing of our ball picker upper" class="w-full rotate-180" />
-            </div>
-        </div>
-    </div>
+	<h1 class="text-center mt-16 text-6xl font-bold">Gallery</h1>
+	<div class="max-w-7xl mx-auto pt-8">
+		<img class="rounded-lg py-4" src="/gallery/dom-and-layla.jpg" alt="hi" />
+		<img class="rounded-lg py-4" src="/gallery/layla-and-cad.jpg" alt="hi" />
+		<img class="rounded-lg py-4" src="/gallery/IMG_2979.jpg" alt="drawing" />
+		<img class="rounded-lg py-4" src="/gallery/radial.jpg" alt="drawing" />
+		<div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto py-8">
+			<img class="rounded-lg py-4" src="/gallery/IMG_2995.jpg" alt="drawing" />
+			<img class="rounded-lg py-4 sm:py-32 -rotate-90" src="/gallery/IMG_2909.jpg" alt="drawing" />
+			<img class="rounded-lg py-4" src="/gallery/addison.jpg" alt="drawing" />
+		</div>
+		<img class="rounded-lg py-4" src="/gallery/shop-discussion.jpg" alt="drawing" />
+		<img class="rounded-lg py-4" src="/gallery/canon/IMG_1440.JPG" alt="drawing" />
+		<div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto py-8 gap-8">
+			<img class="rounded-lg py-4" src="/gallery/canon/IMG_1441.JPG" alt="drawing" />
+			<img class="rounded-lg py-4" src="/gallery/canon/IMG_1444.JPG" alt="drawing" />
+			<img class="rounded-lg py-4" src="/gallery/canon/IMG_1450.JPG" alt="drawing" />
+		</div>
+		<img class="rounded-lg py-4" src="/gallery/canon/IMG_1445.JPG" alt="drawing" />
+		<img class="rounded-lg py-4" src="/gallery/canon/IMG_1448.JPG" alt="drawing" />
+	</div>
 </main>
