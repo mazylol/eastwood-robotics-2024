0f72791eb04e3cb93f72f115e4495262c69d6250
Landon <landonp95@gmail.com>
2024-10-26 14:58:51 -0400

    sponsors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,7 +23,7 @@
 	</div>
 </section>
 <section class="bg-neutral-700 xs:max-w-xl sm:max-w-2xl md:max-w-3xl lg:max-w-7xl mx-auto">
-	<h1 id="therobot" class="text-5xl py-8 text-neutral-200 text-center font-bold">Who We Are</h1>
+	<h1 id="therobot" class="text-7xl py-16 text-neutral-200 text-center font-bold">Who We Are</h1>
 	<enhanced:img
 		src={teamAndRobot}
 		alt="Team and the robot"
@@ -36,4 +36,56 @@
 		club includes students from 6th all the way to 12th grade. We can find a place where anyone can
 		shine.
 	</p>
+</section>
+<section class="bg-neutral-700 mx-auto py-16">
+	<h1 class="text-7xl text-neutral-200 text-center font-bold">Thank you to our sponsors</h1>
+	<div
+		class="pt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:max-w-2xl md:max-w-5xl lg:max-w-7xl mx-auto"
+	>
+		<div class="justify-center">
+			<enhanced:img src="$lib/assets/sponsors/agas.svg" alt="A-Gas" />
+		</div>
+		<div class="justify-center mx-auto">
+			<enhanced:img src="$lib/assets/sponsors/b2b.png" alt="B2B" />
+		</div>
+		<div class="justify-center mx-auto">
+			<enhanced:img src="$lib/assets/sponsors/centaur.png" alt="Centaur Tool & Die" />
+		</div>
+		<div class="justify-center mx-auto">
+			<enhanced:img src="$lib/assets/sponsors/dyco.png" alt="Dyco" />
+		</div>
+		<div class="justify-center mx-auto">
+			<enhanced:img src="$lib/assets/sponsors/energy-harbor.jpg" alt="Energy Harbor" />
+		</div>
+		<div class="justify-center mx-auto w-64">
+			<enhanced:img src="$lib/assets/sponsors/firstsolar.png" alt="First Solar" />
+		</div>
+		<div class="justify-center mx-auto">
+			<enhanced:img src="$lib/assets/sponsors/lfstudios.webp" alt="Life Formations" />
+		</div>
+		<div class="justify-center mx-auto">
+			<enhanced:img src="$lib/assets/sponsors/morrison.jpg" alt="Morrison" />
+		</div>
+		<div class="justify-center mx-auto">
+			<enhanced:img src="$lib/assets/sponsors/napoleonmachine.webp" alt="Napoleon Machine" />
+		</div>
+		<div class="justify-center mx-auto">
+			<enhanced:img src="$lib/assets/sponsors/nsg.png" alt="NSG Group" />
+		</div>
+		<div class="justify-center mx-auto">
+			<enhanced:img src="$lib/assets/sponsors/pbe.png" alt="Principal Business Enterprises" />
+		</div>
+		<div class="justify-center mx-auto">
+			<enhanced:img src="$lib/assets/sponsors/pmmi.png" alt="PMMI" />
+		</div>
+		<div class="justify-center mx-auto">
+			<enhanced:img src="$lib/assets/sponsors/rosenboom.png" alt="Rosenboom" />
+		</div>
+		<div class="justify-center mx-auto">
+			<enhanced:img src="$lib/assets/sponsors/teijin.jpg" alt="Teijin Automotive Technologies" />
+		</div>
+		<div class="justify-center mx-auto">
+			<enhanced:img src="$lib/assets/sponsors/woodcountyhospital.png" alt="Wood County Hospital" />
+		</div>
+	</div>
 </section>
